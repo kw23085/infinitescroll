945267403de41bf42df5a2e3904c2618ac7ff16d
Kevin Wang <kw1017@gmail.com>
2021-04-05 20:29:27 -0700

    make loading works

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const postsContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 6;
 let page = 1;
 
 
@@ -39,5 +39,33 @@ async function showPosts() {
 
 }
 
+// Show loader & fetch more posts
+function showLoading() {
+    loading.classList.add('show');
+    setTimeout(() => {
+        loading.classList.remove('show');
+
+        setTimeout(() => {
+            page++;
+            showPosts();
+        }, 300);
+
+    }, 1000);
+}
+
+
 // Show initial
 showPosts();
+
+
+window.addEventListener('scroll', () => {
+    
+    const { scrollTop, scrollHeight, clientHeight } = 
+    document.documentElement;
+
+
+    if(scrollTop + clientHeight >= scrollHeight - 5) {
+        showLoading();
+    }
+
+});
